d1a78a7371522ee886ecf7110c899b20b404c1b6
Deak Zsolt <lordmairtis@gmail.com>
2016-02-19 10:59:36 +0100

    Add button removed, using detection of last textfield focus. Wrong DOM id generation fixed, now they are all unique. Some minor stylings.

--- DOMBuilder/InputElementBuilder.js
+++ DOMBuilder/InputElementBuilder.js
@@ -1,49 +1,56 @@
+var textDivMaxIndex = -1;
+
 /**
  * Creates a div that contains a textfiled, a plus and a minus button
- * @param UID unique identifier to be added to the default id of the
  * @param {String | undefined} textContent string to be addod to the given new textField as value
  * @returns new div
  */
-function createTextDiv( UID, textContent ) {
+function createTextDiv( textContent ) {
+   textDivMaxIndex++;
    var newTextDiv = document.createElement("DIV");
-   newTextDiv.className = "inputTextDiv form-group";
-   newTextDiv.id = "uriArray"+UID;
+   newTextDiv.className = "inputTextDiv form-group row";
+   newTextDiv.id = "uriArray"+textDivMaxIndex;
    //newTextDiv.innerHTML = "hasznaltauto.hu url:";
    
    //textfield that asks for a car uri and its align-responsible container
-   var textFieldAlignerDiv = document.createElement("DIV");
-   textFieldAlignerDiv.className = "col-sm-10";
    var textField = document.createElement("INPUT");
-   textField.id = uriInputFieldIdPrefix + UID;
+   textField.id = uriInputFieldIdPrefix + textDivMaxIndex;
    textField.type = "url";
-   textField.name = "carUri"+UID;
+   textField.name = "carUri"+textDivMaxIndex;
    textField.className = "form-control";
    textField.placeholder = "hasznaltauto.hu url";
    textField.value = (textContent === undefined) ? "" : textContent;
+   //all textfield has it, but there is to be 10 at maximum so the logic overhead would be greater(always the last one should have it) than this efficiency decrease
+   addEvent( textField, "focus", addOnFocus);
+   
+   var textFieldAlignerDiv = document.createElement("DIV");
+   textFieldAlignerDiv.className = "col-xs-10 col-sm-10 col-sm-10";
    textFieldAlignerDiv.appendChild(textField);
-   textFieldAlignerDiv.style.paddingTop = "2px";
    
    //add a new input field, or remove current
-   var inputButtonPlusDiv = document.createElement("DIV");
-   var inputButtonPlus = document.createElement("BUTTON");
-   //avoid scrolling back to top like with href='#' http://stackoverflow.com/questions/10836428/how-to-maintain-page-scroll-position-after-a-jquery-event-is-carried-out
-   //inputButtonPlus.setAttribute("href", 'javascript:void(0)');
-   inputButtonPlus.className = "btn btn-default btn-sm col-sm-1 form-control-static";
-   inputButtonPlus.type = "button"; //avoid submit, which is default for buttons on forms
-   
-   var inputButtonMinus = inputButtonPlus.cloneNode();
-   
-   inputButtonPlus.innerHTML = "+";
-   inputButtonPlus.id = "inputButtonPlus" + UID;
+   var inputButtonMinus = document.createElement("BUTTON");
+   inputButtonMinus.className = "btn btn-default btn-sm col-xs-1 col-sm-1 col-md-1 form-control-static";
+   inputButtonMinus.type = "button"; //avoid submit, which is default for buttons on forms   
    inputButtonMinus.innerHTML = "-";
-   inputButtonMinus.id = "inputButtonMinus" + UID;
+   inputButtonMinus.id = "inputButtonMinus" + textDivMaxIndex;
    
    newTextDiv.appendChild(textFieldAlignerDiv);
-   newTextDiv.appendChild(inputButtonPlus);
    newTextDiv.appendChild(inputButtonMinus);
    
-   //sessionStorage.inputUriCount++;
    currentInputUriCount++;
    
    return newTextDiv
+}
+
+function addOnFocus(event) {
+   if ( isLastField(event.target) && currentInputUriCount < 10 ) {
+      var addedTextDiv = createTextDiv();
+      formElement.insertBefore(addedTextDiv, sendButtonDiv);
+      event.stopPropagation();
+   }
+}
+
+function isLastField(field) {
+   console.log( field.id && field.id === uriInputFieldIdPrefix + (textDivMaxIndex).toString() );
+    return field.id && field.id === uriInputFieldIdPrefix + (textDivMaxIndex).toString();
 }
--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+//Static vars (used by other js too)
 var uriInputFieldIdPrefix = "uriInputField";
+var currentInputUriCount = 0;
+var formElement = null;
 
 /**
  * addEvent function for crossplatform add event capability
+ * STATIC USE
  * http://stackoverflow.com/questions/6927637/addeventlistener-in-internet-explorer
  */
-function addEvent(elem, evnt, func) {
+function addEvent(elem, type, handler) {
    if (elem.addEventListener) { // W3C DOM
-      elem.addEventListener(evnt,func,false);
+      elem.addEventListener(type,handler,false);
    } else if (elem.attachEvent) { // IE DOM
-      elem.attachEvent("on"+evnt, func);
+      elem.attachEvent("on"+type, handler);
    } else { // No much to do
-      elem[evnt] = func;
+      elem["on" + type] = handler;
    }
 }
 
+//STATIC USE
+//http://stackoverflow.com/questions/12949590/how-to-detach-event-in-ie-6-7-8-9-using-javascript
+function removeEvent(elem, type, handler) {
+   if (elem.removeEventListener) {
+       elem.removeEventListener(type, handler, false);
+   } else if (elem.detachEvent) {
+       elem.detachEvent("on" + type, handler);
+   } else {
+       elem["on" + type] = null;
+   }
+}
+
+
 var sendButtonDiv = null;
-var formElement = null;
-var currentInputUriCount = 0;
 
 /**
  * Saves important user data to sessionStorage before leaving the page
@@ -40,40 +55,23 @@ function windowLoaded(event) {
    //creating the divs which will contain all input fields
    if( !sessionStorage.inputUriCount || sessionStorage.inputUriCount > 9 || sessionStorage.inputUriCount < 1 ) {
      sessionStorage.removeItem
-     var textDiv = createTextDiv(0);
+     var textDiv = createTextDiv();
      formElement.insertBefore(textDiv, sendButtonDiv);
    } else {
       var savedUris = JSON.parse(sessionStorage.inputUriTextContent);
       for(var i = 0; i < Number(sessionStorage.inputUriCount); i++) { 
-         var textDiv = createTextDiv(i, savedUris[i]);
+         var textDiv = createTextDiv(savedUris[i]);
          formElement.insertBefore(textDiv, sendButtonDiv);
       }
    }
    
    //don't add input handler for each button and textfield, propagation check will do just fine
-   addEvent(formElement, "click", addTextDiv);
    addEvent(formElement, "click", deleteTextDiv);
-   addEvent(formElement, "change", inputUriChanged);
-   
+ 
    //for the first  row hide minus button
    document.getElementById("inputButtonMinus0").style.visibility = "hidden";
     
    sendButtonDiv.style.paddingTop = "2px";
-}
-
-/**
- *Eventlistener listens only to roundbuttons that are containing +
- *their parent's parent div is the event listener target.
- *We add a new div with the input fields from the template
-*/
-function addTextDiv(event) {
-   //TODO give proper error message if user tries to make more than 10 textdiv
-   if ( event.target.id && event.target.id.includes("inputButtonPlus") && currentInputUriCount < 10 ) {
-      event.target.blur(); //disallow strucked focus
-      var addedTextDiv = createTextDiv(currentInputUriCount);
-      formElement.insertBefore(addedTextDiv, sendButtonDiv);
-      event.stopPropagation();
-   }
 }
 
 /**
@@ -87,8 +85,4 @@ function deleteTextDiv(event) {
       currentInputUriCount--;
       event.stopPropagation();
     }
-}
-
-function inputUriChanged( event ) {
-    
 }
